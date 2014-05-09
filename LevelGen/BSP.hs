39ec285e47c43f33935568d555a112048051fe04
halvor gb <halvorgb@gmail.com>
2014-05-09 15:18:55 +0200

    better.. getting there. Need better connectivity.

--- LevelGen/BSP.hs
+++ LevelGen/BSP.hs
@@ -4,27 +4,27 @@ import Data.IORef
 import qualified Data.Array as A
 import qualified Data.Array.IO as IOA
 import System.Random
-
+import qualified Data.List as L
 import LevelGen.Model
 
 
 data Direction = Horizontal | Vertical deriving(Eq, Show)
-data BSPTree = Node { nSize :: (Int,Int),
-                      nPosition :: (Int, Int),
-                      nLeftChild :: BSPTree,
+data BSPTree = Node { nLeftChild :: BSPTree,
                       nRightChild :: BSPTree
                     }
 
-             | Leaf
+             | Room { rPosition :: (Int,Int),
+                      rSize :: (Int, Int)
+                    }
                deriving (Show, Eq)
 
 -- Constants:
 
-smallestRoomDimension = 4
+smallestRoomDimension = 6
 
-smallestBSPNodeSize = 8
-largestBSPNodeSize = 19
-largestBSPNodeOffset = 4
+smallestBSPNodeSize = 12
+largestBSPNodeSize = 18
+
 largeRoomChance = 23 -- percent
 
 
@@ -33,43 +33,49 @@ largeRoomChance = 23 -- percent
 bspGen :: (Int, Int) -> IOA.IOArray Position Tile -> IO ()
 bspGen (w,h) level = do
   g <- newStdGen
-  let bspTree = spawnChildren mother g
-  buildRooms bspTree level
 
+  let bspTree = spawnRoomTree (w,h) (0,0) g
   centreList <- newIORef [] :: IO (IORef [(Int, Int)])
-  buildCentreList bspTree centreList
+  buildRooms bspTree level centreList
   centreList' <- readIORef centreList
---  print centreList'
-  connectRooms bspTree level centreList'
+
+  connectRooms level centreList' []
 
   IOA.writeArray level (head centreList') Liquid
   IOA.writeArray level (last centreList') Stairs
 
-    where
-      mother = Node {nSize = (w,h),
-                     nPosition = (0,0),
-                     nLeftChild = undefined,
-                     nRightChild = undefined}
-
 -- private functions
 
-
--- connects two and two rooms, children of the same node.
-connectRooms :: BSPTree -> IOA.IOArray Position Tile -> [(Int, Int)] -> IO ()
-connectRooms tree level [c] = return ()
-connectRooms tree level (c1:c2:cs) = do writeHallway c1 c2
-                                        connectRooms tree level (c2:cs)
+-- connects two and two rooms, for each list member, connect to the nearest room.
+-- TODO: Prevent clustering.
+connectRooms :: IOA.IOArray Position Tile -> [(Int, Int)] -> [(Int, Int)] -> IO ()
+connectRooms _ [] _ = return () -- last iteration
+connectRooms level (c:cs) [] = connectRooms level cs [c] -- first iteration
+connectRooms level centres@(c:cs) visitedCentres
+             = do writeHallway c1 c2
+                  connectRooms level cs (c:visitedCentres)
     where
+      -- find the pair of centres/visitedCentres such that the distance is minimal.
+      (c1, c2,_) = L.foldl' (\acc@(c1, c2, d) cp@(c1',c2',d')  -> if d' < d then cp else acc) ((0,0), (0,0), maxBound :: Int)
+                   [(c1, c2, dist c1 c2) | c1 <- centres, c2 <- visitedCentres]
+
+      dist (x1,y1) (x2, y2) =
+          (abs x2-x1) + (abs y2-y1)
+
       writeHallway :: (Int, Int) -> (Int, Int) -> IO ()
       writeHallway c1'@(x1, y1) c2'@(x2, y2)
           | c1' == c2' = return () -- done
-          | otherwise = do let left  = (x1-1, y1  )
-                               right = (x1+1, y1  )
-                               top   = (x1  , y1-1)
-                               bot   = (x1  , y1+1)
+          | otherwise = do let border = [(x1-1, y1-1), -- topleft
+                                         (x1  , y1-1), -- top
+                                         (x1+1, y1-1), -- topright
+                                         (x1-1, y1  ), -- left
+                                         (x1+1, y1  ), -- right
+                                         (x1-1, y1+1), -- bottomleft
+                                         (x1  , y1+1), -- bottom
+                                         (x1+1, y1+1)] -- bottomleft
 
                            IOA.writeArray level c1' Floor
-                           mapM_ replaceVoid [left,right,top,bot]
+                           mapM_ replaceVoid border
 
                            next
 
@@ -87,126 +93,90 @@ connectRooms tree level (c1:c2:cs) = do writeHallway c1 c2
                 | y2 < y1 = writeHallway (x1, y1-1) c2
                 | otherwise = error "connectRooms: WAT."
 
--- traverses the tree, adds every centre location to the IORef list
-buildCentreList :: BSPTree -> IORef [(Int, Int)] -> IO ()
-buildCentreList tree clIO
-    | l == Leaf &&
-      r == Leaf = do g <- newStdGen
 
-                     let (x,y) = nPosition tree
-                         (w,h) = nSize tree
-                         (centreNoise, _) = randomR (-2,2) g :: (Int, StdGen)
-                         centre@(cx, cy) = (centreNoise + x + w `div` 2, centreNoise + y + h `div` 2)
+-- coordinates here include walls, so if w*h = size, then the room-floor cant be of size w*h
+buildRooms :: BSPTree -> IOA.IOArray Position Tile -> IORef [(Int, Int)] -> IO ()
+buildRooms (Room (x,y) (w,h)) level clIO=
+    do g <- newStdGen
+       let (x', gx) = randomR (x, x+w-smallestRoomDimension) g :: (Int, StdGen)
+           (w', gw) = randomR (smallestRoomDimension, w-(x'-x)) gx :: (Int, StdGen)
+           (y', gy) = randomR (y, y+h-smallestRoomDimension) gw :: (Int, StdGen)
+           (h', gh) = randomR (smallestRoomDimension, h-(y'-y)) gy :: (Int, StdGen)
 
-                     cl <- readIORef clIO
-                     writeIORef clIO (centre:cl)
+           -- // what to draw
+           interior_indices = [(x'', y'') | x'' <- [x'+1..x'+w'-1], y'' <- [y'+1..y'+h'-1]]
 
+           border_indices   = [(x'', y')    | x'' <- [x'..x'+w']] ++ -- top w/corners
+                              [(x'', y'+h') | x'' <- [x'..x'+w']] ++ -- bottom w/corners
+                              [(x' ,   y'') | y'' <- [y'+1..y'+h'-1]] ++   -- left
+                              [(x'+w', y'') | y'' <- [y'+1..y'+h'-1]]      -- right
 
-    | otherwise = do buildCentreList l clIO
-                     buildCentreList r clIO
-      where
-        l = nLeftChild tree
-        r = nRightChild tree
--- fills the interior tiles and wall tiles coresponding to leaf nodes.
-buildRooms :: BSPTree -> IOA.IOArray Position Tile -> IO ()
-buildRooms tree level
-    | l == Leaf &&
-      r == Leaf = do g <- newStdGen
-                     let (x,y) = nPosition tree
-                         (w,h) = nSize tree
-                         maxPadding = max 1 (((min w h) - smallestRoomDimension) `div` 2)
-                         (padding_low, g') = randomR (1, maxPadding) g :: (Int, StdGen)
-                         (padding, _) = randomR (padding_low, maxPadding) g' :: (Int, StdGen) -- try to roll higher
+           -- // where centre is (to draw hallways inbetween nodes)
+           max_centre_noise_x = (w' `div` 2) - 1
+           (centre_noise_x, gcnx) = randomR (-max_centre_noise_x,max_centre_noise_x) gh :: (Int, StdGen)
+           max_centre_noise_y = (h' `div` 2) - 1
+           (centre_noise_y, _) = randomR (-max_centre_noise_y,max_centre_noise_y) gcnx :: (Int, StdGen)
+           centre_x = centre_noise_x + x' + (w' `div` 2)
+           centre_y = centre_noise_y + y' + (h' `div` 2)
 
-                         interior_indices = [(x', y') | x' <- [x+padding..x+w-padding], y' <- [y+padding..y+h-padding]]
+       -- change array at indices
+       mapM_ (\pos -> IOA.writeArray level pos Floor) interior_indices
+       mapM_ (\pos -> IOA.writeArray level pos Wall) border_indices
 
-                         border_indices   = [(x', y+padding-1)   | x' <- [x+padding-1..x+w-padding+1]] ++ -- top w/corners
-                                            [(x', y+h-padding+1) | x' <- [x+padding-1..x+w-padding+1]] ++ -- bottom w/corners
-                                            [(x+padding-1,   y') | y' <- [y+padding..y+h-padding]] ++   -- left
-                                            [(x+w-padding+1, y') | y' <- [y+padding..y+h-padding]]      -- right
-
-                     -- change array at indices
-                     mapM_ (\pos -> IOA.writeArray level pos Floor) interior_indices
-                     mapM_ (\pos -> IOA.writeArray level pos Wall) border_indices
+       -- change centrelist
+       cl <- readIORef clIO
+       writeIORef clIO ((centre_x, centre_y):cl)
 
 
-    | otherwise = do buildRooms l level
-                     buildRooms r level
-
-      where
-        l = nLeftChild tree
-        r = nRightChild tree
-
-
+-- recurse
+buildRooms (Node l r) level clIO = do buildRooms l level clIO
+                                      buildRooms r level clIO
 
 -- spawnChildren builds the BSPTree top down. The undefined values are always written before read.
-spawnChildren :: BSPTree -> StdGen -> BSPTree
-spawnChildren mother g = mother {nLeftChild = l,
-                                 nRightChild = r
-                                }
+spawnRoomTree :: (Int,Int) -> (Int, Int) -> StdGen -> BSPTree
+spawnRoomTree (w,h) (x,y) g
+    | createRoom = Room { rPosition = (x,y),
+                          rSize     = (w, h)
+                        }
+    | otherwise = Node { nLeftChild = spawnRoomTree sl pl gl,
+                         nRightChild = spawnRoomTree sr pr gr
+                       }
     where
-      (w,h) = nSize mother
-      (x,y) = nPosition mother
 
-      -- random direction
-      (d, g') = random g :: (Bool, StdGen)
-      dir :: Direction
-      dir
-          | d     = Horizontal
-          | not d = Vertical
+      -- Decide whether to create a room or to split further:
+      (largeRoomRoll, g_lrr) = randomR (0,100) g :: (Int, StdGen)
+      createLargeRoom = w < largestBSPNodeSize &&
+                        h < largestBSPNodeSize &&
+                        largeRoomRoll < largeRoomChance
 
-      -- whether to make a big room or not:
-      (largeRoomRoll, g'') = randomR (0, 100) g :: (Int, StdGen)
-      largeRoom = largeRoomRoll < largeRoomChance &&
-                  h < largestBSPNodeSize &&
-                    w < largestBSPNodeSize
+      createSmallRoom = w < smallestBSPNodeSize*2 &&
+                        h < smallestBSPNodeSize*2
 
 
+      createRoom = createSmallRoom ||
+                   createLargeRoom
 
 
-      (oX, g''') = randomR (0, largestBSPNodeOffset) g'' :: (Int, StdGen)
-      (oY, g'''') = randomR (0, largestBSPNodeOffset) g''' :: (Int, StdGen)
+      -- for splitting into further BSPTrees:
+      (horizontal_splitRoll, g_hs) = randomR (0,1) g_lrr :: (Int, StdGen)
+      horizontal_split
+          | w < smallestBSPNodeSize*2 = True
+          | h < smallestBSPNodeSize*2 = False
+          | otherwise = horizontal_splitRoll == 0
 
-      (x', y') = (x+oX, y+oY)
-      (w', h') = (w-oX, h-oY)
+      (cut, g_c)
+          | horizontal_split = -- horiz
+              randomR (smallestBSPNodeSize, h - smallestBSPNodeSize) g_hs
+          | otherwise = -- vertical
+              randomR (smallestBSPNodeSize, w - smallestBSPNodeSize) g_hs
 
-      (l,r) = generateChildren dir
+      (sl, pl, sr, pr)
+          | horizontal_split = -- horiz.
+              ((w, cut    ), (x, y),  -- l
+               (w, h-cut), (x, y + cut))  -- r
 
+          | otherwise = -- vertical
+              ((cut, h    ), (x, y),  -- l
+               (w-cut, h), (x + cut, y))  -- r
 
-      generateChildren :: Direction -> (BSPTree, BSPTree)
-      generateChildren d
-          | largeRoom = (Leaf, Leaf) -- Make a Large Room
-          | d == Horizontal =
-              if h' > 2*smallestBSPNodeSize
-              then let (cutY, cg) = randomR (smallestBSPNodeSize, h' - smallestBSPNodeSize) g'''
-                       lm = Node { nSize = (w', cutY),
-                                   nPosition = (x', y'),
-                                   nLeftChild = undefined,
-                                   nRightChild = undefined
-                                 }
-                       rm = lm { nSize = (w', h'- cutY),
-                                 nPosition = (x', y' +cutY)
-                               }
-                       (gl,gr) = split cg
-                   in (spawnChildren lm gl, spawnChildren rm gr) -- Divide room horizontally
-
-              else if w' >= largestBSPNodeSize
-                   then generateChildren Vertical -- Divide the other way
-                   else (Leaf, Leaf) -- Make a small room (no division possible)
-
-          | d == Vertical =
-              if w' > 2*smallestBSPNodeSize
-              then let (cutX, cg) = randomR (smallestBSPNodeSize, w' - smallestBSPNodeSize) g'''
-                       lm = Node { nSize = (cutX, h'),
-                                   nPosition = (x', y'),
-                                   nLeftChild = undefined,
-                                   nRightChild = undefined
-                                 }
-                       rm = lm { nSize = (w' - cutX, h'),
-                                 nPosition = (x' + cutX, y')
-                               }
-                       (gl,gr) = split cg
-                   in (spawnChildren lm gl, spawnChildren rm gr) -- Divide room vertically
-              else if h' >= largestBSPNodeSize
-                   then generateChildren Horizontal -- Divide the other way
-                   else (Leaf, Leaf) -- Make a small room (no division possible)
+      (gl, gr) = split g_c
