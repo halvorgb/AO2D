1ff9edd7063010826ea66063f90f6b878eee0905
halvor gb <halvorgb@gmail.com>
2015-02-01 18:26:08 +0100

    Testing hexagon rendering

--- src/Game/Setup.hs
+++ src/Game/Setup.hs
@@ -47,13 +47,7 @@ gameState =
 unloadedObjects :: UnloadedObjects
 unloadedObjects =
   map fst lamps ++
-  boxes ++
-  [ ObjectUnloaded {  ouPosition    = L.V3 3 1 0
-                   , ouRotation    = L.V3 0 0 0
-                   , ouScale       = L.V3 0.25 0.25 0.25
-                   , ouEntityNames = ["lykt"]
-                   }
-  ]
+  boxes
 
 
 unloadedEntities :: UnloadedEntities
@@ -66,13 +60,21 @@ unloadedEntities =
                    , euGeometryName = "box2"
                    , euMaterialName = "placeholder"
                    }
-  , EntityUnloaded { euUniqueName   = "lykt"
+  , EntityUnloaded { euUniqueName   = "hexagon_white"
                    , euRelativePos  = L.V3 0 0 0
                    , euRelativeRot  = L.V3 0 0 0
                    , euScale        = L.V3 1 1 1
                    , euAmbOverride  = Nothing
-                   , euGeometryName = "lykt"
-                   , euMaterialName = "placeholder"
+                   , euGeometryName = "hexagon"
+                   , euMaterialName = "white"
+                   }
+  , EntityUnloaded { euUniqueName   = "hexagon_gray"
+                   , euRelativePos  = L.V3 0 0 0
+                   , euRelativeRot  = L.V3 0 0 0
+                   , euScale        = L.V3 1 1 1
+                   , euAmbOverride  = Nothing
+                   , euGeometryName = "hexagon"
+                   , euMaterialName = "gray"
                    }
   , EntityUnloaded { euUniqueName   = "light_box"
                    , euRelativePos  = L.V3 0 0 0
@@ -86,7 +88,7 @@ unloadedEntities =
 
 lamps :: [(ObjectUnloaded, PointLight)]
 lamps = [ createLamp (L.V3 (-1) 3 (-1)) (L.V3 0 0 0) (L.V3 0.01 0.01 0.01) "light_box" (L.V3 0.8 0.8 0.8)
---        , createLamp (L.V3 2    4   4)  (L.V3 0 0 0) (L.V3 0.01 0.01 0.01) "light_box" (L.V3 0.8 0.8 0.8)
+        , createLamp (L.V3 2    4   4)  (L.V3 0 0 0) (L.V3 0.01 0.01 0.01) "light_box" (L.V3 0.8 0.8 0.8)
         ]
 createLamp :: Translation -> Rotation -> Scale -> String -> ColorRGB -> (ObjectUnloaded, PointLight)
 createLamp pos rot scale entName clr =
@@ -96,15 +98,25 @@ createLamp pos rot scale entName clr =
 
 
 boxes :: [ObjectUnloaded]
-boxes = [ createBox (L.V3(-0.7) (-0.5) 1.8) zeroRot quarterScale "box2"
-        , createBox (L.V3 0 0 2)            zeroRot quarterScale "box2"
-        , createBox (L.V3 0 1 2)            zeroRot quarterScale "box2"
-        , createBox (L.V3 0 2 2)            zeroRot quarterScale "box2"
-        , createBox (L.V3 0 2 3)            zeroRot quarterScale "box2"
-        , createBox (L.V3 0 (-1) 0)         zeroRot (L.V3 10 0.01 10) "box2"
+boxes = [ createBox (L.V3 0 0 0)                             zeroRot spacingScale "hexagon_white"
+        , createBox (L.V3 (1*0.5*hexLength) 0 (1*0.75*hexWidth)) zeroRot spacingScale "hexagon_white"
+        , createBox (L.V3 (-1*0.5*hexLength) 0 (1*0.75*hexWidth)) zeroRot spacingScale "hexagon_white"
+        , createBox (L.V3 (1*0.5*hexLength) 0 (-1*0.75*hexWidth)) zeroRot spacingScale "hexagon_white"
+        , createBox (L.V3 (-1*0.5*hexLength) 0 (-1*0.75*hexWidth)) zeroRot spacingScale "hexagon_white"
+        , createBox (L.V3 (2*0.5*hexLength) 0 (2*0.75*hexWidth)) zeroRot spacingScale "hexagon_white"
+        , createBox (L.V3 (2*0.5*hexLength) 0 (-2*0.75*hexWidth)) zeroRot spacingScaleTall "hexagon_gray"
         ]
-  where zeroRot      = L.V3 0 0 0
-        quarterScale = L.V3 0.25 0.25 0.25
+  where zeroRot  = L.V3 0 0 0
+        oneScale = L.V3 1 1 1
+        spacingScale     = L.V3 (1.0-spacing) 1.0 (1.0-spacing)
+        spacingScaleTall = L.V3 (1.0-spacing) 2.0 (1.0-spacing)
+
 createBox :: Translation -> Rotation -> Scale -> String -> ObjectUnloaded
 createBox pos rot scale entName =
   ObjectUnloaded pos rot scale [entName]
+
+
+hexWidth, hexLength, spacing :: GLfloat
+hexLength = 0.87
+hexWidth = 1
+spacing = 0.1
